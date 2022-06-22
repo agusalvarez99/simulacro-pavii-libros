ff587589c5c46fb6dcded63c97a2cc88e1a9749e
agusalvarez99 <agusalvarez99@hotmail.com>
2022-06-22 11:33:38 -0300

    NO ESTA TERMINADO

--- src/app/components/libros/libros.component.ts
+++ src/app/components/libros/libros.component.ts
@@ -1,15 +1,70 @@
 import { Component, OnInit } from '@angular/core';
+import { LibrosService } from '../../services/libros.service';
+import { Libros } from '../../models/libros';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-libros',
   templateUrl: './libros.component.html',
-  styleUrls: ['./libros.component.css']
+  styleUrls: ['./libros.component.css'],
 })
 export class LibrosComponent implements OnInit {
-
-  constructor() { }
-
+  titulo: string = 'Libros';
+  TituloAccionABMC = {
+    A: '(Agregar)',
+    B: '(Eliminar)',
+    M: '(Modificar)',
+    C: '(Consultar)',
+    L: '(Listado)',
+  };
+  AccionABMC: string = 'L';
+  submitted = false;
+  OpcionesActivo = [
+    { Id: null, Nombre: '' },
+    { Id: true, Nombre: 'SI' },
+    { Id: false, Nombre: 'NO' },
+  ];
+  Items: Libros[] = [];
+  constructor(private librosService: LibrosService) {}
+  FormRegistro = new FormGroup({
+    Id: new FormControl(0),
+    Titulo: new FormControl('', [
+      Validators.required,
+      Validators.maxLength(100),
+    ]),
+    Stock: new FormControl('', [
+      Validators.required,
+      Validators.pattern('[0-9]{1,3}'),
+    ]),
+    Activo: new FormControl(true),
+  });
   ngOnInit() {
+    this.Buscar();
+  }
+  Buscar() {
+    this.AccionABMC = 'L';
+    this.librosService.get().subscribe((res: Libros[]) => {
+      this.Items = res;
+    });
+  }
+  Modificar(Item) {
+    this.submitted = false;
+    this.FormRegistro.markAsUntouched();
+    this.BuscarPorId(Item, 'M');
+  }
+  BuscarPorId(Item, AccionABMC) {
+    window.scroll(0, 0); // ir al incio del scroll
+
+    this.librosService.getById(Item.Id).subscribe((res: any) => {
+      this.FormRegistro.patchValue(res);
+      this.AccionABMC = AccionABMC;
+    });
   }
 
+  Volver() {
+    this.AccionABMC = 'L';
+  }
+  Consultar(Item) {
+    this.BuscarPorId(Item, 'C');
+  }
 }
--- src/app/services/libros.service.ts
+++ src/app/services/libros.service.ts
@@ -8,7 +8,7 @@ import { Libros } from '../models/libros';
 export class LibrosService {
   resourceUrl: string;
   constructor(private httpClient: HttpClient) {
-    this.resourceUrl = 'http://pav2.azurewebsites.net/api/libros';
+    this.resourceUrl = 'https://pav2.azurewebsites.net/api/libros';
   }
   get() {
     return this.httpClient.get(this.resourceUrl);
@@ -20,7 +20,7 @@ export class LibrosService {
   getById(Id: number) {
     return this.httpClient.get(this.resourceUrl + Id);
   }
-  put(Id: number, obj: any) {
+  put(Id: number, obj: Libros) {
     return this.httpClient.put(this.resourceUrl + Id, obj);
   }
 }
